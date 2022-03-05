6994a8ecc4917e68c3c37b5ad692ee2e8f13d75a
Michael Bernhart <michael.bernhart@oberalp.com>
2022-03-05 20:24:07 +0100

    Initial Commit

---
+++ Tutors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutors
+//
+//  Created by OakHost Customer on 05/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutors/TutorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorsApp.swift
+//  Tutors
+//
+//  Created by OakHost Customer on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TutorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
