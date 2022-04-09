0603755ccada316578ccd5bdf3366f618a014a66
Michael Bernhart <michael.bernhart@oberalp.com>
2022-04-09 12:52:24 +0200

    First Commit

--- Tutors/ContentView.swift
+++ Tutors/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image(systemName: "photo")
+            VStack(alignment: .leading) {
+                Text("Michael Bernhart")
+                Text("Evi Gruber")
+            }
+        }
     }
 }
 
